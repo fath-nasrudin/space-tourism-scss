bed8255159178917afebe389291aa3e6a9808846
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-04-04 20:50:47 +0700

    Add tabs interactive functionality

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,62 @@ navToggle.addEventListener('click', (e) => {
 
   }
 })
+
+/* ----------------
+  tabs
+-------------------*/
+const tablist = document.querySelector('[role="tablist"]');
+const tabs = tablist.querySelectorAll('[role="tab"]');
+
+// focus switch
+tablist.addEventListener('keydown', switchFocus);
+
+let currentTabFocus = 0;
+function switchFocus(e) {
+  const currentKey = e.key;
+  const KeyLeft = 'ArrowLeft';
+  const KeyRight = 'ArrowRight';
+
+  if (currentKey === KeyLeft || currentKey === KeyRight) {
+    tabs[currentTabFocus].setAttribute('tabindex', -1);
+
+    if (currentKey === KeyLeft) {
+      currentTabFocus--;
+      if (currentTabFocus < 0) currentTabFocus = tabs.length - 1;
+    } else if (currentKey === KeyRight) {
+      currentTabFocus++;
+      if (currentTabFocus >= tabs.length) currentTabFocus = 0;
+    }
+
+    tabs[currentTabFocus].setAttribute('tabindex', 0);
+    tabs[currentTabFocus].focus();
+  }
+}
+
+// click tab listeners
+tabs.forEach(tab => {
+  tab.addEventListener('click', (e) => {
+    const currentTab = e.target;
+    const panelId = currentTab.getAttribute('aria-controls');
+    const imageId = currentTab.getAttribute('data-tab-image');
+    const tablist = currentTab.closest('[role="tablist"]');
+    const mainContainer = tablist.parentElement;
+
+
+    // change the focus
+    Array.from(tablist.children).forEach(tab => tab.setAttribute('aria-selected', false))
+    currentTab.setAttribute('aria-selected', true);
+
+    // change the content
+    mainContainer
+      .querySelectorAll('[role="tabpanel"]')
+      .forEach(panel => panel.setAttribute('hidden', true));
+    mainContainer.querySelector(`#${panelId}`).removeAttribute('hidden');
+
+    // change the image
+    mainContainer
+      .querySelectorAll('[data-tab-image-item]')
+      .forEach(panel => panel.setAttribute('hidden', true));
+    mainContainer.querySelector(`#${imageId}`).removeAttribute('hidden');
+  })
+})
